72cc990d9df0b71e3d63a63f1ba3a8b0c6cb5013
nurazom <88337901+nurazom@users.noreply.github.com>
2021-12-22 10:38:09 +0600

    app bar added

---
+++ lib/pages/note_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({Key? key}) : super(key: key);
+
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Note List"),
+      ),
+    );
+  }
+}
